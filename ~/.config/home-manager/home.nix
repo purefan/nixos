e62b26a72e5c04ad0c25c5521794762d6ad93379
purefan <purefan@outlook.com>
2023-06-14 14:29:00 +0200

    initial state

---
+++ ~/.config/home-manager/home.nix
@@ -0,0 +1,52 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "purefan";
+  home.homeDirectory = "/home/purefan";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "23.05";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  home.packages = [
+    pkgs.htop
+    pkgs.vscode
+    pkgs.brave 
+    pkgs.tigervnc # enable emulate middle figer in input
+    pkgs.ferdium
+    pkgs.git
+    pkgs.terminator
+    pkgs.nextcloud-client
+    pkgs.keepassxc
+    pkgs.obsidian
+    pkgs.scid-vs-pc
+    pkgs.gparted
+    pkgs.obs-studio # 12/06/2023
+    pkgs.spotify # 13/06/2023
+    pkgs.gtop # requested by https://cinnamon-spices.linuxmint.com/applets/view/79
+    # 14/06/2023
+    pkgs.nodenv # nvm didnt work within 5 minutes
+    pkgs.nix-index # needed for nix-locate
+  ];
+
+  programs.vscode = {
+    enable = true;
+    extensions = with pkgs.vscode-extensions; [
+        dbaeumer.vscode-eslint
+        ms-vscode-remote.remote-ssh
+        ms-vsliveshare.vsliveshare
+    ];
+  };
+
+}
